2e46df9ba8a208ece3ddef3ca399d57cd9ce09ff
Michael <mckeever02@gmail.com>
2015-03-09 14:58:54 +0000

    Minified js

--- moodmemo/js/main.js
+++ moodmemo/js/main.js
@@ -152,3 +152,16 @@ jQuery(document).ready(function($){
 		$newWord.removeClass('is-hidden').addClass('is-visible');
 	}
 });
+
+//SVG fallback
+if (!Modernizr.svg) {
+      var imgs = document.getElementsByTagName('img');
+      var svgExtension = /.*\.svg$/
+      var l = imgs.length;
+      for(var i = 0; i < l; i++) {
+          if(imgs[i].src.match(svgExtension)) {
+              imgs[i].src = imgs[i].src.slice(0, -3) + 'png';
+              console.log(imgs[i].src);
+          }
+      }
+  };
