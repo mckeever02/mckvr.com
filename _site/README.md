9369ccc3573a792479f5088497cfdcd0d85d85af
Michael <mckeever02@gmail.com>
2020-11-01 15:24:54 +0000

    Add _site

---
+++ _site/README.md
@@ -0,0 +1,26 @@
+`snappy-jekyll` is a setup for making snappy static sites.
+
+* Jekyll for static site generation
+* Webpack for CSS and JS asset compilation
+* Tailwind as a utility CSS framework
+* Turbolinks for performance
+
+## Install Workflow Dependencies
+
+```
+$ gem install bundler # if not already installed
+$ npm -g install yarn
+```
+
+## Install Application Dependencies
+
+```
+$ bundle
+$ yarn
+```
+
+## Run Build Process
+
+```
+$ yarn start
+```
---
+++ _site/webpack/entry.js
@@ -0,0 +1,22 @@
+// const Turbolinks = require("turbolinks")
+// Turbolinks.start()
+
+import './scss_files/main.scss';
+
+const scrollButton = document.querySelector('.scroll-top');
+scrollButton.addEventListener('click', function(){
+    window.scrollTo(0, 0);
+})
+
+let scrollpos = window.scrollY
+  const screenHeight = screen.height;
+  const add_class_on_scroll = () => scrollButton.classList.add("scale-90")
+  const remove_class_on_scroll = () => scrollButton.classList.remove("scale-90")
+
+  window.addEventListener('scroll', function() {
+    scrollpos = window.scrollY;
+
+    if (scrollpos >= screenHeight) { add_class_on_scroll() }
+    else { remove_class_on_scroll() }
+
+  })
